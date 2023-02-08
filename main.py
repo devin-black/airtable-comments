9ca8d8b65091bbeb3b29f8f9f39d1ea0c329601b
Devin Black <82221522+devin-black@users.noreply.github.com>
2023-02-07 20:02:22 -0500

    Logging adjustment

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import requests
 from dotenv import load_dotenv
 from slack_sdk import WebhookClient
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.INFO)
 
 
 def request_api(url: str, headers: dict, timeout: int = 10) -> requests.Response:
